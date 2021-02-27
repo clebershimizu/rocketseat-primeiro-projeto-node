98ed5eade1f40964cea8d5d8f59fd20847f6b9b8
clebershimizu <cleber.shimizu@gmail.com>
2021-02-27 06:05:38 -0300

    Improves appointment generation route

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express';
+import { v4 as uuid } from 'uuid';
 
 const appointmentsRouter = Router();
+const appointments = [];
 
 appointmentsRouter.post('/', (request, response) => {
-  return response.json({ message: 'Hello Helsinki!' });
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
--- src/server.ts
+++ src/server.ts
@@ -3,17 +3,8 @@ import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
-
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-  const user = {
-    name,
-    email,
-  };
-
-  response.json(user);
-});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333.');
